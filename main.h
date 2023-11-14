f4523a79d2a4fd9b72a9f8c4052c7fbdcc36d910
Rugwiroparfait <rugwiroparfait003@gmail.com>
2023-11-14 12:03:40 +0200

    print_string_non_printacle() added

--- main.h
+++ main.h
@@ -26,6 +26,6 @@ int print_unsigned(va_list args);
 int print_octal(va_list args);
 int print_hex(va_list args);
 int print_string_custom(va_list args);
-
+int print_string_non_printable(va_list args);
 #endif
 
---
+++ task6.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_string_non_printable - Prints a string with non-printable characters
+ * @args: The argument list
+ * Return: Number of characters printed
+ */
+int print_string_non_printable(va_list args)
+{
+    char *str = va_arg(args, char *);
+    int count = 0;
+
+    /* Implement logic to print string with non-printable characters */
+
+    return count;
+}
